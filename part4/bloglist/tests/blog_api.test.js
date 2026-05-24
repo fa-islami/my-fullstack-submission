83ef510a5d5a4c595ec3de4fbfbd063804816ea9
fa-islami <falahislami@gmail.com>
2026-05-24 23:50:41 +0800

    finished doing 4.10 - 4.12 (4.6 - 4.7 skipped)

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -30,4 +30,54 @@ test('blogs has unique identifier named "id"', async () => {
   assert(response.body[0].id);
 });
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'BLOG4',
+    author: 'Bogex',
+    url: 'http://example.com/blog4',
+    likes: 16,
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const blogAtEnd = await helper.blogsInDb();
+  assert.strictEqual(blogAtEnd.length, helper.initialBlogs.length + 1);
+
+  const titles = blogAtEnd.map(b => b.title);
+  assert(titles.includes('BLOG4'));
+});
+
+test('the default value of likes property is 0', async () => {
+  const newBlog = {
+    title: 'BLOG5',
+    author: 'Sompot',
+    url: 'http://example.com/blog5',
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const blogAtEnd = await helper.blogsInDb();
+  const index = blogAtEnd.findIndex(b => b.title === 'BLOG5');
+
+  assert.strictEqual(blogAtEnd[index].likes, 0);
+});
+
+test('blog with invalid data is not added', async () => {
+  const invalidBlog = {
+    author: 'Sarif',
+    url: 'http://example.com/blog6',
+    likes: 9,
+  };
+
+  await api.post('/api/blogs').send(invalidBlog).expect(400);
+});
+
 after(async () => await mongoose.connection.close());
